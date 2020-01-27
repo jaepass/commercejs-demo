d5db758e7aefd989564852a4eee99cff41afd725
Jaeriah Tay <jaeriahpasseri@gmail.com>
2020-01-27 14:08:46 -0800

    installed vue-router, created cart route

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@
 import Commerce from '@chec/commerce.js';
 
 import Vue from 'vue';
+import Router from 'vue-router';
 import App from './App.vue';
+
 
 // Initialize store with public key, store key in variable
 const commerce = new Commerce('pk_17695092cf047ebda22cd36e60e0acbe5021825e45cb7', true);
 
 Vue.config.productionTip = false;
+
+Vue.use(Router);
 
 // Create a Vue instance
 // Pass our Commerce instance as a prop
@@ -15,3 +19,17 @@ new Vue({
   render: h => h(App,
     { props: { commerce } }),
 }).$mount('#app');
+
+// Add routes
+export default new Router({
+  routes: [
+    {
+      path: '/',
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('@/components/Cart'),
+    },
+  ],
+});
